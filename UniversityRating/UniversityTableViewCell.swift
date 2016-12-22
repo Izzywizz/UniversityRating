653cc70e5a348feb9e23494b6b7340a4ab97a521
Izzywizz <izzywizzz@gmail.com>
2016-12-22 13:46:29 +0000

    New button image imported from andy and only button is able to be selected

--- UniversityRating/UniversityTableViewCell.swift
+++ UniversityRating/UniversityTableViewCell.swift
@@ -9,21 +9,30 @@
 import UIKit
 
 class UniversityTableViewCell: UITableViewCell {
-
+    
     //MARK: Stored Properties
     @IBOutlet weak var moduleLabel: UILabel!
     var universitObject = University()
     
+    @IBOutlet weak var poorButton: UIButton!
+    @IBOutlet weak var notGreatButton: UIButton!
+    @IBOutlet weak var satisfactoryButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var amazingButton: UIButton!
+    var buttonArray: [UIButton] = [UIButton]() // Empty UIButton array
     
     //MARK: nib Methods
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+        setupEmoji()
+        buttonArray = [poorButton, notGreatButton, satisfactoryButton, goodButton, amazingButton] // Buttons have now loaded in the view
 
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
@@ -32,18 +41,54 @@ class UniversityTableViewCell: UITableViewCell {
     func configureCell() {
         moduleLabel.text = "What do you think of Module \(universitObject.module)"
     }
-
+    
     //MARK: Action Methods
     @IBAction func emojiSelected(_ sender: UIButton) {
         
         switch sender.tag {
-        case 0: print("POOR")
-        case 1: print("NotGreat")
+        case 0:
+            print("POOR")
+            offSelectedButtonState()
+            sender.isSelected = !sender.isSelected;
+        case 1:
+            print("NotGreat")
+            offSelectedButtonState()
+            sender.isSelected = !sender.isSelected;
         case 2: print("Satisfactory")
         case 3: print("Good")
         case 4: print("Amazing")
         default:
             print("TAG not reconized")
         }
+        
+        print(sender.tag)
+    }
+    
+    //MARK: Emoji Image Setup
+    func setupEmoji() {
+        //set normal image
+        poorButton.setImage(UIImage.init(named: "POOR"), for: .normal)
+        notGreatButton.setImage(UIImage.init(named: "NOT GREAT"), for: .normal)
+        
+        //set highlighted image
+        poorButton.setImage(UIImage.init(named: "POOR-A"), for: .selected)
+        notGreatButton.setImage(UIImage.init(named: "NOT GREAT-A"), for: .selected)
+    }
+
+    func offSelectedButtonState() {
+        for button in buttonArray {
+            if button.isSelected == true {
+                button.isSelected = false
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
